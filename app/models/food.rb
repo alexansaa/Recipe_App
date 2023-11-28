9a8d9603f8e13dddd14469f89ff5f592bcff54cc
unknown <macherki.nessrine@yahoo.com>
2023-11-28 17:34:04 +0100

    add linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-    validates :name, :measurement_unit, :price, presence: true
-    has_many :recipe_foods, dependent: :destroy
+  validates :name, :measurement_unit, :price, presence: true
+  has_many :recipe_foods, dependent: :destroy
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,5 +1,5 @@
 class RecipeFood < ApplicationRecord
-    belongs_to :recipe
-    belongs_to :food
-    validates :quantity, numericality: {only_integer: true}
+  belongs_to :recipe
+  belongs_to :food
+  validates :quantity, numericality: { only_integer: true }
 end
