507a98bb09a396372abbbc21d605279703118e9a
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-28 20:18:59 +0100

    Update Food model with associations to Recipe, Inventory

--- db/migrate/20231128152125_create_foods.rb
+++ db/migrate/20231128152125_create_foods.rb
@@ -1,9 +1,10 @@
 class CreateFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :foods do |t|
-      t.string :name 
-      t.integer :measurement_unit
-      t.integer :price
+      t.string :name
+      t.string :measurement_unit
+      t.float :price
+
       t.timestamps
     end
   end
