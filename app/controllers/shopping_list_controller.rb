00ab7e4950a61cc51a8e964abc4a74eac49019d2
alexansaa <alexan.saa@gmail.com>
2023-11-29 18:40:32 -0500

    linter fix

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -10,12 +10,4 @@ class ShoppingListController < ApplicationController
 
     @missing_food_items = recipe_food_items - inventory_food_items
   end
-
-  private
-
-  def calculate_missing_foods(user)
-    # logic to calculate missing foods
-    # Should show the list of food that is missing for all recipes of the logged-in user (compare the list of food for all recipes with the general food list of that user).
-    # Should count the total food items and total price of the missing food.
-  end
 end
