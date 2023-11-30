521bd5a64d6a1ccf0de51e3cbb55efb9f1a92b57
unknown <macherki.nessrine@yahoo.com>
2023-11-30 21:39:06 +0100

    add routes and fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  resources :recipes, only: [:show]
-  resources :inventories, only: [:show]
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Route for displaying recipes
+  get '/recipes', to: 'recipes#index', as: 'recipes'
+
+  # Route for displaying inventories
+  get '/inventories', to: 'inventories#index', as: 'inventories'
+
+  # Route for displaying foods
+  get '/foods', to: 'foods#index', as: 'foods'
+
+  # Route for displaying public recipes
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+
+  # Route for editing user profile
+  get '/edit_user_registration', to: 'users#edit', as: 'edit_user_registration'
+
+  # Route for destroying user session (logging out)
+  delete '/destroy_user_session', to: 'sessions#destroy', as: 'destroy_user_session'
 end
