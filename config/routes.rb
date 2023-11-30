bfbb5a90fd62523bca33156c3531aff62ba3631f
unknown <macherki.nessrine@yahoo.com>
2023-11-30 21:13:13 +0100

    added Menu bar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,19 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :recipes
+  get '/inventories', to: 'inventories#index', as: 'inventories'
+
+  # Route for displaying foods
+  get '/foods', to: 'foods#index', as: 'foods'
+
+  # Route for displaying public recipes
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+
+  # Route for editing user profile
+  get '/edit_user_registration', to: 'users#edit', as: 'edit_user_registration'
+
+  # Route for destroying user session (logging out)
+  delete '/destroy_user_session', to: 'sessions#destroy', as: 'destroy_user_session'
   # Defines the root path route ("/")
   # root "posts#index"
 end
