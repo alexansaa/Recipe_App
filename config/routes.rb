1b7386326daf4c69a214459fe9cfc57d1a03a2c0
unknown <macherki.nessrine@yahoo.com>
2023-11-30 21:41:42 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  # Route for displaying recipes
-  get '/recipes', to: 'recipes#index', as: 'recipes'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Route for displaying inventories
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :recipes
   get '/inventories', to: 'inventories#index', as: 'inventories'
 
   # Route for displaying foods
@@ -16,4 +19,6 @@ Rails.application.routes.draw do
 
   # Route for destroying user session (logging out)
   delete '/destroy_user_session', to: 'sessions#destroy', as: 'destroy_user_session'
+  # Defines the root path route ("/")
+  # root "posts#index"
 end
